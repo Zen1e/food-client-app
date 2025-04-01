fd1c9caaf591ef6273a325c5e6551803a8ff0268
Zen1e <monkhdelger.edts@gmail.com>
2025-04-01 17:19:08 +0800

    ip

--- src/components/Foods.tsx
+++ src/components/Foods.tsx
@@ -2,31 +2,33 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import CatFoods from "./CatFoods";
 
-export default function Foods({orderList,setOrderList}){
-    const [catList, setCatList] = useState([]);
+export default function Foods({ orderList, setOrderList }) {
+  const [catList, setCatList] = useState([]);
 
-    useEffect(() => {
-      const fetchCategory = async () => {
-        try {
-          const response = await axios.get("https://food-service-app-ciba.onrender.com/cat/");
-          setCatList(response.data.cats);
-        } catch (err) {
-          console.log(err);
-        }
-      };
-      fetchCategory();
-    }, []);
+  useEffect(() => {
+    const fetchCategory = async () => {
+      try {
+        const response = await axios.get(
+          "https://food-service-app-ciba.onrender.com/cat/"
+        );
+        setCatList(response.data.cats);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchCategory();
+  }, []);
 
-
-    return(
-        <div className="w-[1440px]">
-            {catList?.map((el,index) => (
-                <div key={index} className="mb-[100px]">
-                    <div className="text-[30px] font-bold mb-[60px]" id={el.catName}>{el.catName}</div>
-                    <CatFoods category={el.catName} orderList={orderList} setOrderList={setOrderList}/>
-                </div>
-            ))
-            }
-        </div>
-    )
+  return (
+    <div className="w-[1440px]">
+      {catList?.map((el, index) => (
+        <CatFoods
+          key={index}
+          category={el.catName}
+          orderList={orderList}
+          setOrderList={setOrderList}
+        />
+      ))}
+    </div>
+  );
 }
